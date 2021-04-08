3bc60982a139e040de530f6a68267591e081cffc
MarceloSouzaEDM <markandy128@gmail.com>
2021-04-08 10:29:14 -0300

    consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserLogin } from '../model/UserLogin';
+import { Usuario } from '../model/Usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private http : HttpClient
+
   constructor() { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin)
+  }
+
+  cadastrar(user: Usuario): Observable<Usuario>{
+      return this.http.post<Usuario>('http://localhost:8080/usuario/cadastrar', user)
+  }
 }
+
